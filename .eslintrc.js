b35dc1decb10e4570b0c97efa4ca824d17645eab
KarolisVaitiekunas <karolisvaitiekunas1@gmail.com>
2022-05-25 16:34:25 +0300

    Fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,23 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  extends: ['plugin:react/recommended', 'google', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
+    ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'require-jsdoc': 'off',
   },
   settings: {
     react: {
-      version: '17.0.2',
+      version: 'latest',
     },
   },
 };
